121ab246e8175a157012d5325daa7198e58f1aa3
adeyahya <adeyahyaprasetyo@gmail.com>
2021-02-16 23:16:27 +0700

    update ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,102 +1,21 @@
-import { useState, useRef } from "react";
-import { Container, Stack, Button, Heading, Input } from "@chakra-ui/react";
+import { Stack, Button, Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
-  const [connectionId, setConnectionId] = useState(null);
-  const [roomId, setRoomId] = useState("");
-  const [joinedCall, setJoinedCall] = useState(false);
-  const videoRef = useRef(null);
-  const peerRef = useRef(null);
-
-  const handleCall = () => {
-    // @ts-ignore
-    const peer = new Peer();
-    const getUserMedia = navigator.getUserMedia;
-    getUserMedia(
-      { video: true, audio: true },
-      function (stream) {
-        const call = peer.call(roomId, stream);
-        call.on("stream", function (remoteStream) {
-          setJoinedCall(true);
-          videoRef.current.srcObject = remoteStream;
-        });
-      },
-      function (err) {
-        console.log("Failed to get local stream", err);
-      }
-    );
-  };
-
-  const connect = () => {
-    const getUserMedia = navigator.getUserMedia;
-    // @ts-ignore
-    peerRef.current = new Peer();
-    const peer = peerRef.current;
-
-    peer.on("open", function () {
-      if (peer.id) {
-        console.log("connection id", peer.id);
-        setConnectionId(peer.id);
-      }
-    });
-
-    // startCapture().then((stream) => {
-    //   console.log(stream);
-    //   videoRef.current.srcObject = stream;
-
-    //   peer.on("call", function (call) {
-    //     call.answer(stream);
-    //   });
-    // });
-
-    peer.on("call", function (call) {
-      getUserMedia(
-        { video: true, audio: true },
-        function (stream) {
-          call.answer(stream);
-        },
-        function (err) {
-          console.log("Failed to get local stream", err);
-        }
-      );
-
-      call.on("stream", function (remoteStream) {
-        videoRef.current.srcObject = remoteStream;
-      });
-    });
-  };
-
-  // function startCapture() {
-  //   // @ts-ignore
-  //   return navigator.mediaDevices.getDisplayMedia(null);
-  // }
-
   return (
-    <Container maxW="5xl">
-      <Stack
-        display={connectionId || joinedCall ? "none" : undefined}
-        minH="100vh"
-        spacing="3rem"
-        justify="center"
-        align="center"
-      >
-        <Heading>Google Meet Clone</Heading>
-        <Stack direction="row">
-          <Input
-            value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
-            type="text"
-            placeholder="room id"
-          />
-          <Button onClick={handleCall} colorScheme="pink">
-            Call
-          </Button>
-        </Stack>
-        <Button onClick={connect} colorScheme="blue" size="lg">
-          Become Host
-        </Button>
+    <Stack
+      spacing="2rem"
+      position="relative"
+      align="center"
+      justify="center"
+      h="100%"
+    >
+      <Stack>
+        <Heading textAlign="center">Rumah Kumpul</Heading>
+        <Text>Tempat semua orang berkumpul ria</Text>
       </Stack>
-      <video ref={videoRef} autoPlay controls />
-    </Container>
+      <Button size="lg" colorScheme="pink">
+        Buat Ruangan
+      </Button>
+    </Stack>
   );
 }
--- pages/room/[slug].tsx
+++ pages/room/[slug].tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack, Heading, Avatar, Wrap, WrapItem } from "@chakra-ui/react";
 
-const dummyData = Array(10).fill("");
+const dummyData = Array(5).fill("");
 const dummyListeners = Array(5).fill("");
 
 export const RoomDetail: React.FC = () => {
@@ -13,11 +13,16 @@ export const RoomDetail: React.FC = () => {
         </Stack>
         <Stack>
           <Box>
-            <Heading size="md">Speakers ({dummyData.length})</Heading>
+            <Heading size="sm">Speakers ({dummyData.length})</Heading>
           </Box>
-          <Wrap spacing="1.5rem" justify="left" align="center">
+          <Wrap spacing="1.5rem" justify="flex-start" align="center">
             {dummyData.map((_, index) => (
-              <WrapItem alignItems="center" w="calc(25% - 1.5rem)" key={index}>
+              <WrapItem
+                alignItems="center"
+                justifyContent="center"
+                minW="calc(25% - 1.5rem)"
+                key={index}
+              >
                 <Stack>
                   <Avatar size="lg" />
                   <Heading size="xs">Speaker {index + 1}</Heading>
@@ -29,11 +34,16 @@ export const RoomDetail: React.FC = () => {
 
         <Stack>
           <Box>
-            <Heading size="md">Listeners ({dummyListeners.length})</Heading>
+            <Heading size="sm">Listeners ({dummyListeners.length})</Heading>
           </Box>
-          <Wrap spacing="1.5rem" justify="left" align="center">
+          <Wrap spacing="1.5rem" justify="flex-start" align="center">
             {dummyListeners.map((_, index) => (
-              <WrapItem alignItems="center" w="calc(25% - 1.5rem)" key={index}>
+              <WrapItem
+                alignItems="center"
+                justifyContent="center"
+                minW="calc(25% - 1.5rem)"
+                key={index}
+              >
                 <Stack>
                   <Avatar size="lg" />
                   <Heading size="xs">Listener {index + 1}</Heading>
