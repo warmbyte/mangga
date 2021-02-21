8223542dc75d62dbff9f3dae45f96d2eba7fe5f0
adeyahya <adeyahyaprasetyo@gmail.com>
2021-02-21 12:11:31 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <script defer src="/peerjs.js"></script>
+          <script defer src="/peer.js"></script>
         </Head>
         <body>
           <Main />
